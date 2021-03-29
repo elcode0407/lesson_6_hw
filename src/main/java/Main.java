4d3f6800b06668b80e18feb90bc925b3d80655d9
elcode13 <elkhanjafarov07@gmail.com>
2021-03-29 12:50:52 +0400

    Java Level 1 Lesson 6 Homework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,22 @@ import animals.Dog;
 
 public class Main {
     public static void main(String[] args) {
-        Animals[] animals = {new Cat(200,50,0),new Cat(200,50,0),new Dog(500,23,10),new Cat(200,50,0)};
+        Animals[] animals = {new Cat(200, 50, 0), new Cat(200, 50, 0), new Dog(500, 23, 10), new Cat(200, 50, 0)};
         int a = 0;
         int b = 0;
         for (Animals animal : animals) {
             System.out.println(animal.getName()
-            + "\n" + animal.run(501)
-            + "\n" + animal.jump(51)
-            + "\n" + animal.swim(32));
+                    + "\n" + animal.run(501)
+                    + "\n" + animal.jump(51)
+                    + "\n" + animal.swim(32));
             if (animal.getName().equals("Dog:")) {
                 b++;
-            } else if(animal.getName().equals("Cat:")){
+            } else if (animal.getName().equals("Cat:")) {
                 a++;
             }
         }
+
+
         System.out.println("Dog: " + b);
         System.out.println("Cat: " + a);
 
